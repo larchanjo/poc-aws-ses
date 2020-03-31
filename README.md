d374c75f833fd96b6006d5f09da4104545da03d1
Luram Archanjo <luram.archanjo@sensedia.com>
2020-03-30 21:45:36 -0300

    Initial Project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Overview
+
+This is a poc using [AWS SDK](https://aws.amazon.com/sdk-for-java/) and [AWS SES (Simple Email Service)](https://aws.amazon.com/ses/).
+
+# Setup AWS
+
+* [IAM](https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html)
+* [AWS SES (Simple Email Service)](https://docs.aws.amazon.com/ses/latest/DeveloperGuide/sign-up-for-aws.html)
+
+# Setup
+
+You need to configure the following environment variables:
+
+* FROM_EMAIL
+* TO_EMAIL
+* AWS_ACCESS_KEY_ID
+* AWS_SECRET_ACCESS_KEY
+
+# Be Happy
---
+++ src/main/java/com/example/aws/ses/Application.java
@@ -0,0 +1,76 @@
+package com.example.aws.ses;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.simpleemail.model.SendEmailResult;
+import java.util.Objects;
+
+public class Application {
+
+  static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+  static final String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+      + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+      + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+      + "AWS SDK for Java</a>";
+
+  static final String TEXTBODY = "This email was sent through Amazon SES "
+      + "using the AWS SDK for Java.";
+
+  public static void main(String[] args) {
+    String from = System.getenv("FROM_EMAIL");
+    if (Objects.isNull(from)) {
+      throw new RuntimeException("Missing FROM_EMAIL environment variable");
+    }
+
+    String to = System.getenv("TO_EMAIL");
+    if (Objects.isNull(to)) {
+      throw new RuntimeException("Missing TO_EMAIL environment variable");
+    }
+
+    String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+    if (Objects.isNull(accessKey)) {
+      throw new RuntimeException("Missing AWS_ACCESS_KEY_ID environment variable");
+    }
+
+    String accessSecret = System.getenv("AWS_SECRET_ACCESS_KEY");
+    if (Objects.isNull(accessSecret)) {
+      throw new RuntimeException("Missing AWS_SECRET_ACCESS_KEY environment variable");
+    }
+
+    try {
+      AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder
+          .standard()
+          .withRegion(Regions.US_WEST_2)
+          .build();
+
+      Destination destination = new Destination()
+          .withToAddresses(to);
+
+      Body body = new Body()
+          .withHtml(new Content().withCharset("UTF-8").withData(HTMLBODY))
+          .withText(new Content().withCharset("UTF-8").withData(TEXTBODY));
+
+      Message message = new Message()
+          .withBody(body)
+          .withSubject(new Content().withCharset("UTF-8").withData(SUBJECT));
+
+      SendEmailRequest request = new SendEmailRequest()
+          .withDestination(destination)
+          .withMessage(message)
+          .withSource(from);
+
+      SendEmailResult response = client.sendEmail(request);
+      System.out.println(String.format("Email=[%s] sent from=[%s] to=[%s]", response, from, to));
+    } catch (Exception ex) {
+      System.out.println("The email was not sent. Error message: " + ex.getMessage());
+    }
+  }
+
+}
